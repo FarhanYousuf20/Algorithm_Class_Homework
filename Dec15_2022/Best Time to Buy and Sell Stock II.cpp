ee4f22b3cfca52f2da17295f1e4d78d656c0be61
FarhanYousuf20 <76511419+FarhanYousuf20@users.noreply.github.com>
2022-12-18 19:42:59 +0600

    Create Best Time to Buy and Sell Stock II.cpp

---
+++ Dec15_2022/Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,12 @@
+// Problem link : https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0, n = prices.size();
+        for(int i = 1; i < n; i++) {
+            if(prices[i] > prices[i - 1]) ans += (prices[i] - prices[i - 1]);
+        }
+        return ans;
+    }
+};
